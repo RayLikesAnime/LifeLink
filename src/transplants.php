fac19dd6c94efbdce2803b22a1d67f307b106024
Keshav Pandey <88835211+RayLikesAnime@users.noreply.github.com>
2023-05-09 10:03:22 +0530

    changes

--- src/transplants.php
+++ src/transplants.php
@@ -43,7 +43,7 @@ echo "<script type='text/javascript'>alert('$message');</script>";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Patient</title>
+    <title>Transplants</title>
 </head>
 <body>
 <header>
